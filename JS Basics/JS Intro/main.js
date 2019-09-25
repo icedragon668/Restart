21861a889cd84c34c9cb3b34d8bb9ddb6b5ae183
Kori <icedragon668@gmail.com>
2019-09-24 21:48:59 -0400

    adds first math JS, and notation on alerts

--- JS Basics/JS Intro/main.js
+++ JS Basics/JS Intro/main.js
@@ -1,5 +1,22 @@
 console.log('sup')
 
 function MyFunction() {
-    alert('oy!')
+    let num
+    num = 7482/281
+    //I am pretty clearly rusty on the basics
+    //I cannot remember how to do this without template literals
+    // alert(`7482 divided by 281 is ${num}`);
+    // alert('blah', num); //nope
+    //alert(num + 'blah') //nope
+    //damn, had to look it up :p
+    alert('7482 divided by 281 is ' + num);
 }
+
+
+/*
+1. 7482 divided by 281
+2. 3.14 times 5 divided by 3
+3. The remainder of 281 divided by 9 (You may need to look up the modulo operator)
+4. 18 divided by 0
+5. The remainder of 10 divided by 0 (You may need to look up the modulo operator)
+*/
