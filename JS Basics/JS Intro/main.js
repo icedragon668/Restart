923b0a4fed47de9d779d1f243e98d62a585c3a66
Kori <icedragon668@gmail.com>
2019-09-24 21:58:07 -0400

    adds math3 and math3 bonus

--- JS Basics/JS Intro/main.js
+++ JS Basics/JS Intro/main.js
@@ -12,11 +12,13 @@ function MyFunction() {
     //leaving the "incorrect" in as a reminder
     alert('7482 divided by 281 is ' + num);
     alert('3.14 times 5 divided by 3 is ' + 3.14 * 5 /3)
+    alert('the remainder of 281 divided by 9 is ' + 281 % 9)
+    //fun! i haven't used parseInt before ;p
+    alert('281 divided by 9 is ' + parseInt(281/9) + ' remainder ' + 281%9)
 }
 
 
 /*
-2. 3.14 times 5 divided by 3
 3. The remainder of 281 divided by 9 (You may need to look up the modulo operator)
 4. 18 divided by 0
 5. The remainder of 10 divided by 0 (You may need to look up the modulo operator)
