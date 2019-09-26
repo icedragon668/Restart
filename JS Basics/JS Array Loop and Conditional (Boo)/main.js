5e26633f86fcfb751dfd7da9f25588aab3edfbd7
Kori <icedragon668@gmail.com>
2019-09-25 21:55:32 -0400

    adds tasks to code

---
+++ JS Basics/JS Array Loop and Conditional (Boo)/main.js
@@ -0,0 +1,11 @@
+// 1
+
+//empty array
+//prompt 5 names
+//alert name 2
+//prompt for index
+//alert name at index
+//array with 5 names
+//promt for a name
+//alert true/false for student present
+
