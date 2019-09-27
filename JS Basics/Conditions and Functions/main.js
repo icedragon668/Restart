50de4cbda79325f615fc40b9e13e834e07705dea
Kori <icedragon668@gmail.com>
2019-09-26 22:36:17 -0400

    adds block 5 instructions

--- JS Basics/Conditions and Functions/main.js
+++ JS Basics/Conditions and Functions/main.js
@@ -51,3 +51,44 @@ In this activity we will create 3 functions.
 Be sure to call all of your functions after defining them and log all of your results in the console.
 
 */
+
+/* Block 5
+refactor:
+const numList1 = [1, 4, 5, 6, 2, 3, 1, 7, 6, 8];
+const numList2 = [10, 9, 77, 9, 2, 1, 0, 82, 2];
+const numList3 = [1, 14, 66, 543, 2, 1, 5, 674];
+const numList4 = [9, 6, 3, 2, 1, 6, 7, 8, 8, 7];
+const numList5 = [4.2, 5.1, 3, 0, 0, 453, 2.43];
+
+let sum = 0;
+
+for (let i = 0; i < numList1.length; i++){
+  sum = sum + numList1[i];
+}
+
+console.log(sum);
+
+sum = 0;
+
+for (let i = 0; i < numList2.length; i++){
+  sum = sum + numList2[i];
+}
+
+console.log(sum);
+
+sum = 0;
+
+for (let i = 0; i < numList3.length; i++){
+  sum = sum + numList3[i];
+}
+
+console.log(sum);
+
+sum = 0;
+
+for (let i = 0; i < numList4.length; i++){
+  sum = sum + numList4[i];
+}
+
+console.log(sum);
+*/
