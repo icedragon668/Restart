f0bd726f0b916491f490bf62da9df60c5f61b7e1
Kori <icedragon668@gmail.com>
2019-09-26 22:36:55 -0400

    updates block 5 instructions

--- JS Basics/Conditions and Functions/main.js
+++ JS Basics/Conditions and Functions/main.js
@@ -53,6 +53,15 @@ Be sure to call all of your functions after defining them and log all of your re
 */
 
 /* Block 5
+You'll now do what's called `refactoring` which means to restructure existing code. 
+
+Open up `index.html` and create a new function that will accept an array as a paremeter.
+
+What this function will do is take the parameter passed in and loop over it.
+
+As it looks through the numbers, it will add up all of the numbers in the array, and return the sum of all of the numbers.
+
+After you finish, call the new function 5 times, passing in a different array each time.
 refactor:
 const numList1 = [1, 4, 5, 6, 2, 3, 1, 7, 6, 8];
 const numList2 = [10, 9, 77, 9, 2, 1, 0, 82, 2];
