abfec96a77f929b12352df6184861258204fdbc3
Kori <icedragon668@gmail.com>
2019-09-26 23:31:11 -0400

    adds arrays to block 5

--- JS Basics/Conditions and Functions/main.js
+++ JS Basics/Conditions and Functions/main.js
@@ -67,6 +67,12 @@ alert(evenness(7) + ": 7 is even")
 
 // Block 5
 
+const numList1 = [1, 4, 5, 6, 2, 3, 1, 7, 6, 8];
+const numList2 = [10, 9, 77, 9, 2, 1, 0, 82, 2];
+const numList3 = [1, 14, 66, 543, 2, 1, 5, 674];
+const numList4 = [9, 6, 3, 2, 1, 6, 7, 8, 8, 7];
+const numList5 = [4.2, 5.1, 3, 0, 0, 453, 2.43];
+
 const arraySum = (array) => {
     let sum;
     for (i=0; i < array.length; i++){
@@ -88,11 +94,6 @@ As it looks through the numbers, it will add up all of the numbers in the array,
 
 After you finish, call the new function 5 times, passing in a different array each time.
 refactor:
-const numList1 = [1, 4, 5, 6, 2, 3, 1, 7, 6, 8];
-const numList2 = [10, 9, 77, 9, 2, 1, 0, 82, 2];
-const numList3 = [1, 14, 66, 543, 2, 1, 5, 674];
-const numList4 = [9, 6, 3, 2, 1, 6, 7, 8, 8, 7];
-const numList5 = [4.2, 5.1, 3, 0, 0, 453, 2.43];
 
 let sum = 0;
 
