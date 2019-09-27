2689f6fbbdf7fc8f9149ba92e3e1af7c051a34e4
Kori <icedragon668@gmail.com>
2019-09-26 22:33:17 -0400

    adds block 4 instructions

--- JS Basics/Conditions and Functions/main.js
+++ JS Basics/Conditions and Functions/main.js
@@ -1,5 +1,3 @@
-//  2
-
 //Block 1
 const classList = ['Math', 'Science', 'English', 'Music', 'Intro to Programming', 'Basketweaving'];
 /*
@@ -38,4 +36,18 @@ Finally, call your function passing in any argument you wish.
 ## Challenge
 
 Re-write the `multiplyFunc` so that it will console log the taken parameter multiplied by `3`.
+*/
+
+/* Block 4
+Open your editor and create a file named `function.js`.
+
+In this activity we will create 3 functions.
+
+1. Our first function will accept 1 parameter (a number) and multiply it by itself and return the new value.
+2. Our next function will accept 2 parameters (both strings: a first name and a last name) and return the name in the following format: lastName, firstName. 
+  * e.g. given the arguments ("Arya", "Stark"), the function should return "Stark, Arya".
+3. Our last function will accept 1 parameter (number) and return a boolean: true if the number is even, and false if the number is false.
+
+Be sure to call all of your functions after defining them and log all of your results in the console.
+
 */
