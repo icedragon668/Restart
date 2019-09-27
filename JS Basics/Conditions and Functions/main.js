e3a9be2413df9e654c1e8dd5549a01c35e115373
Kori <icedragon668@gmail.com>
2019-09-26 22:29:38 -0400

    adds instructions for block 1

--- JS Basics/Conditions and Functions/main.js
+++ JS Basics/Conditions and Functions/main.js
@@ -1,1 +1,16 @@
-//  1
+//  2
+
+//Block 1
+const classList = ['Math', 'Science', 'English', 'Music', 'Intro to Programming', 'Basketweaving'];
+/*
+Using the given `classList` array: 
+
+1. Prompt the user for the name of a class they would like to update.
+2. Prompt the user for a new class name.
+3. Replace the old class name with the new one.
+
+## Challenge:
+
+Do the above without writing a for loop.
+*/
+
