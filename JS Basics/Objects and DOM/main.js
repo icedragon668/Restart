984dc2e6151ce0e0195632da93488e9e75578af1
Kori <icedragon668@gmail.com>
2019-09-27 23:01:23 -0400

    adds function 5 from 01

--- JS Basics/Objects and DOM/main.js
+++ JS Basics/Objects and DOM/main.js
@@ -2,3 +2,10 @@ const multiply = function (a,b) {console.log(a*b)}
 const add = function (a,b) {console.log(a+b)}
 const concat = function(a,b) {console.log(String(a)+String(b))}
 const fiveLong = function(a) {console.log(Boolean(a.length>5))}
+const sumArray = function(a) {
+    let sum = 0;
+    for (i=0; i < a.length; i++) {
+        sum += a[i]
+    }
+    console.log(sum)
+}
