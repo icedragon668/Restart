bcec5f2e23e503dda4665abb08f64c23c073618f
Kori <icedragon668@gmail.com>
2019-09-27 22:58:32 -0400

    adds function 4 from 01

--- JS Basics/Objects and DOM/main.js
+++ JS Basics/Objects and DOM/main.js
@@ -1,3 +1,4 @@
 const multiply = function (a,b) {console.log(a*b)}
 const add = function (a,b) {console.log(a+b)}
 const concat = function(a,b) {console.log(String(a)+String(b))}
+const fiveLong = function(a) {console.log(Boolean(a.length>5))}
