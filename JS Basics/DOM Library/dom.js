3b88d2192c6628a4d6fc25b901f3cf496d08e3d1
Kori <icedragon668@gmail.com>
2019-10-01 22:15:41 -0400

    adds removeClass function

--- JS Basics/DOM Library/dom.js
+++ JS Basics/DOM Library/dom.js
@@ -1,47 +1,53 @@
 const $ = function () {
-  // Block 1 //
-  //select every "content" id and replace the text
-  //select every "content" class
-  const nodeList = document.querySelectorAll('.content');
+    // Block 1 //
+    //select every "content" id and replace the text
+    //select every "content" class
+    const nodeList = document.querySelectorAll('.content');
 
     //sets innerText to text(content) for .content class elements
-  const text = function(content){
-    for (let i = 0; i < nodeList.length; i++) {
-      nodeList[i].innerText = content;
+    const text = function (content) {
+        for (let i = 0; i < nodeList.length; i++) {
+            nodeList[i].innerText = content;
+        }
+    };
+
+    //Block 2//
+    //add 2 <p> to each .content
+    const html = function (c) {
+        for (i = 0; i < nodeList.length; i++) {
+            nodeList[i].innerHTML = c;
+        }
     }
-  };
-  
-  //Block 2//
-  //add 2 <p> to each .content
-  const html = function (c){
-      for (i=0;i<nodeList.length;i++){
-          nodeList[i].innerHTML = c;
-      }
-  }
-  //Block 3//
-  //add many functions
-  
-  //addClass: adds a class to .content
-  const addClass = function (c){
-      for (i=0; i<nodeList.length; i++) {
-          nodeList[i].classList.add(c)
-      }
-  }
-  /*
-    removeClass: removes a class from .content
-    toggleClass: add/remove a class from .content
-    empty: empties .content's innerHTML
-    append: adds to .content (add, NOT replace)
-    prepend: adds to the begining of .content
-    val: takes optional string, or gets the value of an input
-      ??? phrasing
-    on: takes a string and function: the string represents an event, the function an callback
-  */
+    //Block 3//
+    //add many functions
+
+    //addClass: adds a class to .content
+    const addClass = function (c) {
+        for (i = 0; i < nodeList.length; i++) {
+            nodeList[i].classList.add(c)
+        }
+    }
+    //removeClass: removes a class from .content
+    const removeClass = function (c) {
+        for (i = 0; i < nodeList.length; i++) {
+            nodeList[i].classList.remove(c)
+        }
+    }
+    /*
+        toggleClass: add/remove a class from .content
+        empty: empties .content's innerHTML
+        append: adds to .content (add, NOT replace)
+        prepend: adds to the begining of .content
+        val: takes optional string, or gets the value of an input
+          ??? phrasing
+        on: takes a string and function: the string represents an event, the function an callback
+      */
 
     //allows use of text()
     return {
-    text: text,
-    html: html,
-    addClass: addClass,
-  };
+        text: text,
+        html: html,
+        addClass: addClass,
+        removeClass: removeClass,
+    };
 }
