2ba83152c617cefe5f62e68a61d91fd4c72669b0
Kori <icedragon668@gmail.com>
2019-10-01 21:59:33 -0400

    adds new function to dom.js

--- JS Basics/DOM Library/dom.js
+++ JS Basics/DOM Library/dom.js
@@ -1,18 +1,27 @@
-// Block 1 //
-//select every "content" id and replace the text
 const $ = function () {
+  // Block 1 //
+  //select every "content" id and replace the text
   //select every "content" class
   const nodeList = document.querySelectorAll('.content');
 
     //sets innerText to text(content) for .content class elements
-    const text = function(content){
+  const text = function(content){
     for (let i = 0; i < nodeList.length; i++) {
       nodeList[i].innerText = content;
     }
   };
   
+  //Block 2//
+  //add 2 <p> to each .content
+  const html = function (c){
+      for (i=0;i<nodeList.length;i++){
+          nodeList[i].innerHTML = c;
+      }
+  }
+
     //allows use of text()
     return {
     text: text,
+    html: html,
   };
 }
