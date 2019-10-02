0a6161789fc3f09047fbc26d33447a318e5b60da
Kori <icedragon668@gmail.com>
2019-10-01 21:53:10 -0400

    adds notation

--- JS Basics/DOM Library/app.js
+++ JS Basics/DOM Library/app.js
@@ -1,1 +1,9 @@
-$().text('This is added text, boing');
+$().text('This is added text');
+
+/*
+what the $%^& was that??
+I wrote the EXCACT same code four *&^% times...
+no clue what happened there...
+I guess that is an important reminder:
+"Sometimes, you just have to start over"
+*/
