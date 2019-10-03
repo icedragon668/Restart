24ebf38856d8344eb4cf6f6aa250ddaaf9824683
Kori <icedragon668@gmail.com>
2019-10-02 21:55:26 -0400

    adds a canvas, attempts to fit to screen

--- Fun Stuff/RPGy/app.js
+++ Fun Stuff/RPGy/app.js
@@ -1,1 +1,10 @@
 $(5).test(6)
+
+const canvas = document.querySelectorAll("canvas")
+
+const canvasSizer = function () {
+    canvas.innerHeight = window.innerHeight
+    canvas.innerWidth = window.innerWidth
+}
+
+canvasSizer()
