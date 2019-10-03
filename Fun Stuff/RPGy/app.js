bd0d72048b887c2ead1245e8f620a3b29cc5157c
Kori <icedragon668@gmail.com>
2019-10-02 22:03:53 -0400

    adjusts to make canvas fit to screen, reactive not tested

--- Fun Stuff/RPGy/app.js
+++ Fun Stuff/RPGy/app.js
@@ -2,9 +2,10 @@ $(5).test(6)
 
 const canvas = document.querySelector("canvas")
 
+//not reactive, needs an "on-resize" or similar
 const canvasSizer = function () {
-    canvas.height = window.innerHeight-2
-    canvas.width = window.innerWidth-2
+    canvas.height = window.innerHeight-1
+    canvas.width = window.innerWidth-1
 }
 
 canvasSizer()
