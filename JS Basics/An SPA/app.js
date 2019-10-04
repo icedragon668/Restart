611536fbcb91ea709e65c8f20b1569e443d62d3b
Kori <icedragon668@gmail.com>
2019-10-03 22:28:08 -0400

    adds some functions to dom.js

---
+++ JS Basics/An SPA/app.js
@@ -0,0 +1,1 @@
+$('#homepage').text('Rewrite')
--- JS Basics/An SPA/dom.js
+++ JS Basics/An SPA/dom.js
@@ -1,6 +1,18 @@
-const $ = function () {
+const $ = function (s) {
+    
+    const nL = document.querySelectorAll(s);
+    
+    text = (c) => {
+        nL.forEach(e=>{
+            e.innerText = c;
+    })}
+
+    html = (c) => {
+        nL.forEach(e=>{
+            e.innerHTML = c;
+    })}
 
     return {
-        
+        text: text,
     }
 }
