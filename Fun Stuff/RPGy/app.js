c8c335f14faa41e5dadb5b9fbb9c86623ee4254e
Kori <icedragon668@gmail.com>
2019-10-03 23:20:17 -0400

    adds arc notation

--- Fun Stuff/RPGy/app.js
+++ Fun Stuff/RPGy/app.js
@@ -97,10 +97,14 @@ function draw () {
     ctx.beginPath();
     ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
     ctx.moveTo(110, 75);
-    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise) //half circle
     ctx.moveTo(65, 65);
     ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
     ctx.moveTo(95, 65);
     ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
     ctx.stroke();
+
+    //arc(x,y, r, sA,eA, c)
+    //arc ( x,y of circle, with radius of r, starting angle, ending angle, anti-clockwise)
+    //in general, if anti-clockwise (true), it can be left off
 }
