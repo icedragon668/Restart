be4510ae0400047e04d136eb83135eb810fdebd3
Kori <icedragon668@gmail.com>
2019-10-03 23:06:09 -0400

    adds a square to canvas

--- Fun Stuff/RPGy/app.js
+++ Fun Stuff/RPGy/app.js
@@ -69,5 +69,9 @@ const fight = function () {
     `
 }
 
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+function draw () {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10,10,50,50);
+}
