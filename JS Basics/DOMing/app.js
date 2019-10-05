8b20e318681778517a8e12ed229777b2377fdab9
Kori <icedragon668@gmail.com>
2019-10-04 21:22:31 -0400

    adds an arry to use to app.js

---
+++ JS Basics/DOMing/app.js
@@ -0,0 +1,14 @@
+const drinkList = [
+    'Coffee: $5',
+    'Espresso: $7',
+    'Cappuccino: $6',
+    'Latte: $4',
+    'Tea: $3',
+    'Ice Coffee: $6',
+    'Ice Espresso: $8',
+    'Ice Latte: $6',
+    'Ice Tea: $4'
+  ];
+
+  //using the dom lib, append this to the DOM
+
