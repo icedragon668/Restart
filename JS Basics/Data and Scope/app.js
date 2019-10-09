b1229420b161bc7aa83b8a16faaac198e2e9c80a
Kori <icedragon668@gmail.com>
2019-10-08 22:04:24 -0400

    refactors instruction 4

--- JS Basics/Data and Scope/app.js
+++ JS Basics/Data and Scope/app.js
@@ -43,9 +43,9 @@ const todoList =[
   // 2. Append `pick up art supplies` to the screen to the `#groceries` div. 
 //  document.querySelectorAll('#groceries').forEach(e=>e.innerHTML+=todoList[0].tasks.errands.personal[0])
   // 3. Append `diet coke` to the screen to the `#errands` div. 
- document.querySelectorAll('#errands').forEach(e=>e.innerHTML+=todoList[0].tasks.grocery.drinks.sodas[1])
+//  document.querySelectorAll('#errands').forEach(e=>e.innerHTML+=todoList[0].tasks.grocery.drinks.sodas[1])
   // 4. Append `hair appt  at 2:15pm` to the screen to the `#calendar` div. 
- document.querySelectorAll('#calendar').forEach(e=>e.innerHTML+=`<p>${todoList[0].tasks.dueToday.personal[0].task} at ${todoList[0].tasks.dueToday.personal[0].time}</p>`)
+//  document.querySelectorAll('#calendar').forEach(e=>e.innerHTML+=`<p>${todoList[0].tasks.dueToday.personal[0].task} at ${todoList[0].tasks.dueToday.personal[0].time}</p>`)
   // 5. Update the time to `submit expense report` to `8:30am` and append the results to the `#calendar` div.
  todoList[0].tasks.dueToday.work[1].time = `8:30am`
  document.querySelectorAll('#calendar').forEach(e=>e.innerHTML+=`<p>${todoList[0].tasks.dueToday.work[1].task} at ${todoList[0].tasks.dueToday.work[1].time}</p>`)
@@ -71,5 +71,6 @@ const append = function(s,c){
 append('#groceries', `<p>${toDo.grocery.produce[0]}</p>`)
 append('#groceries', `<p>${toDo.errands.personal[0]}</p>`)
 append('#errands', `<p>${toDo.grocery.drinks.sodas[1]}</p>`)
+append('#calendar', `<p>${toDo.dueToday.personal[0].task} at ${toDo.dueToday.personal[0].time}</p>`)
 
 // append('#errands', `<p>${toDo}</p>`)
