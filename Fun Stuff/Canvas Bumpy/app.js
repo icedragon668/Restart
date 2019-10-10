1dac381090feda93c3dad610be4df64bd391a9ec
Kori <icedragon668@gmail.com>
2019-10-09 21:40:08 -0400

    slows down the animation

--- Fun Stuff/Canvas Bumpy/app.js
+++ Fun Stuff/Canvas Bumpy/app.js
@@ -1,7 +1,7 @@
 //preps an image to be loaded to canvas
 let img = new Image();
 img.src = 'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png'
-img.onload = function () {init()}
+img.onload = function () { init() }
 
 //sets up canvas and context
 let canvas = document.querySelector('canvas')
@@ -14,14 +14,14 @@ const sHeight = height * scale
 
 //animation starts here!
 //simplifies drawImage to frames over pixels
-function drawFrame (frameX, frameY, canvasX, canvasY){
+function drawFrame(frameX, frameY, canvasX, canvasY) {
     ctx.drawImage(img,
         //1 will use the first sprite image, eg
         //source
         frameX * width, frameY * height, width, height,
         //destination
         canvasX, canvasY, sWidth, sHeight
-        );
+    );
 }
 
 function init() {
@@ -36,27 +36,38 @@ function init() {
     dest width,
     dest height,
     */
-   drawFrame(0,0,0,0) //sprite 1, pos 1
-   drawFrame(1,0, sWidth, 0); //sprite 2, pos 2
-   drawFrame(0,0, sWidth*2,0); //sprite 1, pos 3
-   drawFrame(2,0, sWidth*3,0); //sprite 3, pos 4
+
+    //animation requests
+    drawFrame(0, 0, 0, 0) //sprite 1, pos 1
+    window.requestAnimationFrame(step);
+    /*
+    drawFrame(0, 0, 0, 0) //sprite 1, pos 1
+    drawFrame(1, 0, sWidth, 0); //sprite 2, pos 2
+    drawFrame(0, 0, sWidth * 2, 0); //sprite 1, pos 3
+    drawFrame(2, 0, sWidth * 3, 0); //sprite 3, pos 4
+    */
 }
 
-//animation requests
-window.requestAnimationFrame(step);
 
 //variables for animation
 const cycleLoop = [0, 1, 0, 2];
 let currentLoopIndex = 0;
+let frameCount = 0;
 
 function step() {
-    ctx.clearRect(0,0,canvas.width, canvas.height) //clears canvas
-    drawFrame(cycleLoop[currentLoopIndex], 0,0,0) //loops sprite 1,2,1,3 at pos 1
+    frameCount++;
+    if (frameCount < 15) { //counts 15 frames before next image
+        //go again!
+        window.requestAnimationFrame(step)
+        return;
+    }
+    frameCount = 0
+    ctx.clearRect(0, 0, canvas.width, canvas.height) //clears canvas
+    drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0) //loops sprite 1,2,1,3 at pos 1
     currentLoopIndex++; //cycle increment
     //checks to see if loop is ended
-    if (currentLoopIndex >= cycleLoop.length){
+    if (currentLoopIndex >= cycleLoop.length) {
         currentLoopIndex = 0
     }
-    //go again!
     window.requestAnimationFrame(step)
 }
