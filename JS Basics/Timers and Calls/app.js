21ae4612b804a72bd21dc87589fdc7bdfdbb47c2
Kori <icedragon668@gmail.com>
2019-10-10 22:42:35 -0400

    adds setimeout, fails

--- JS Basics/Timers and Calls/app.js
+++ JS Basics/Timers and Calls/app.js
@@ -20,14 +20,15 @@ const friendList = [
       status: 'offline'
     }
   ]
-  
 
+  setTimeout(getFriends, 3000)
   /*
   ## Instructions
 
-The activity mimics waiting for a database response before we gather up the relevant data and render it to the page.
+Use a loop to get all the names from the friendList after 3 seconds.
+ Append each name to a string surrounded by paragraph tags.
+  Your string should look something like `<p>Sally</p><p>Jim</p>`.
 
-Use a loop to get all the names from the friendList after 3 seconds. Append each name to a string surrounded by paragraph tags. Your string should look something like `<p>Sally</p><p>Jim</p>`.
-
-Once you have all the names, use the render function to render the whole string to the page.
+Once you have all the names, use the render function to render
+ the whole string to the page.
 */
