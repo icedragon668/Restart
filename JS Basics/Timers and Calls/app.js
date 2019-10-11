e7a0585d2a7bb645f3b8d51cb88fb1a6a6130674
Kori <icedragon668@gmail.com>
2019-10-10 22:51:26 -0400

    adds block 2 instructions

--- JS Basics/Timers and Calls/app.js
+++ JS Basics/Timers and Calls/app.js
@@ -48,3 +48,17 @@ Use a loop to get all the names from the friendList after 3 seconds.
 Once you have all the names, use the render function to render
  the whole string to the page.
 END BLOCK 1*/
+
+//BLOCK 2
+
+/*
+## Instructions
+
+Now that we're getting the hang of things happening asynchronously, let's try a tougher one. 
+
+This is similar to the last exercise, but like last class, we are now getting our data from somewhere else and can't see it until it arrives. 
+
+Use a loop to get all the movies from the incoming data after 1 second. Append each movies to a string surrounded by paragraph tags. Your string should look something like `<p>the matrix</p><p>the godfather</p>`.
+
+Once you have all the names, add a render function that renders the whole string to the page.
+END BLOCK 2*/
