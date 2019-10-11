91e0e4618c47609a69ede2a98b6ca41fc2efccb3
Kori <icedragon668@gmail.com>
2019-10-10 23:04:29 -0400

    adds block 3 instructions

--- JS Basics/Timers and Calls/app.js
+++ JS Basics/Timers and Calls/app.js
@@ -79,3 +79,25 @@ Use a loop to get all the movies from the incoming data after 1 second.
 Once you have all the names, add a render function that renders
  the whole string to the page.
 END BLOCK 2*/
+
+//Block 3
+
+/*
+## Instructions
+
+* Go to the [iexTrading API](https://iextrading.com/developer/docs)
+ then use the documentation to try to answer the following:
+
+1. How would I query the iexTrading API to get all of the stock
+ information related to Apple Inc.?
+
+2. How would I query the iexTrading API to get all of the stock
+ information related to Microsoft Inc.?
+
+## Challenge
+
+* Using the documentation, figure out how you would retrieve the
+ prices of multiple stocks in one.
+
+* _Hint:_ The solution isn't in the `Price` section of the documentation.
+END BLOCK 3 */
