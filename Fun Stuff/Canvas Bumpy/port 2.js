8215fc07ae4d91cf3d63f87ec9e7db9be424aafe
Kori <icedragon668@gmail.com>
2019-10-11 23:37:35 -0400

    attempts a different map, fails

--- Fun Stuff/Canvas Bumpy/port 2.js
+++ Fun Stuff/Canvas Bumpy/port 2.js
@@ -1,57 +1,130 @@
 var map = {
-    cols: 8,
-    rows: 8,
+    cols: 12,
+    rows: 12,
     tsize: 64,
     layers: [[
-        3, 3, 3, 3, 3, 3, 3, 3,
-        3, 1, 1, 1, 1, 1, 1, 3,
-        3, 1, 1, 1, 1, 2, 1, 3,
-        3, 1, 1, 1, 1, 1, 1, 3,
-        3, 1, 1, 2, 1, 1, 1, 3,
-        3, 1, 1, 1, 2, 1, 1, 3,
-        3, 1, 1, 1, 2, 1, 1, 3,
-        3, 3, 3, 1, 2, 3, 3, 3
+        3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3,
+        3, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 3,
+        3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 3,
+        3, 3, 3, 1, 1, 2, 3, 3, 3, 3, 3, 3
     ], [
-        4, 3, 3, 3, 3, 3, 3, 4,
-        4, 0, 0, 0, 0, 0, 0, 4,
-        4, 0, 0, 0, 0, 0, 0, 4,
-        4, 0, 0, 5, 0, 0, 0, 4,
-        4, 0, 0, 0, 0, 0, 0, 4,
-        4, 0, 0, 0, 0, 0, 0, 4,
-        4, 4, 4, 0, 5, 4, 4, 4,
-        0, 3, 3, 0, 0, 3, 3, 3
+        4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 5, 0, 0, 0, 0, 0, 5, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 4, 4, 0, 5, 4, 4, 4, 4, 4, 4, 4,
+        4, 4, 4, 0, 0, 3, 3, 3, 3, 3, 3, 3
     ]],
     getTile: function (layer, col, row) {
         return this.layers[layer][row * map.cols + col];
     }
 };
 
+function Camera(map, width, height) {
+    this.x = 0;
+    this.y = 0;
+    this.width = width;
+    this.height = height;
+    this.maxX = map.cols * map.tsize - width;
+    this.maxY = map.rows * map.tsize - height;
+}
+
+Camera.SPEED = 256; // pixels per second
+
+Camera.prototype.move = function (delta, dirx, diry) {
+    // move camera
+    this.x += dirx * Camera.SPEED * delta;
+    this.y += diry * Camera.SPEED * delta;
+    // clamp values
+    this.x = Math.max(0, Math.min(this.x, this.maxX));
+    this.y = Math.max(0, Math.min(this.y, this.maxY));
+};
+
 Game.load = function () {
     return [
-        Loader.loadImage('tiles', '../../assests/images/tiles.png'),
-        // Loader.loadImage('character', 'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png')
-        Loader.loadImage('character', '../../assests/images/toon.png')
+        Loader.loadImage('tiles', '../../assets/images/tiles.png'),
     ];
 };
 
 Game.init = function () {
+    Keyboard.listenForEvents(
+        [Keyboard.LEFT, Keyboard.RIGHT, Keyboard.UP, Keyboard.DOWN]);
     this.tileAtlas = Loader.getImage('tiles');
-    this.hero = {x: 128, y: 384, image: Loader.getImage('character')};
+    this.camera = new Camera(map, 512, 512);
+
+    // create a canvas for each layer
+    this.layerCanvas = map.layers.map(function () {
+        var c = document.createElement('canvas');
+        c.width = 512;
+        c.height = 512;
+        return c;
+    });
+
+    // initial draw of the map
+    this._drawMap();
+};
+
+Game.update = function (delta) {
+    this.hasScrolled = false;
+    // handle camera movement with arrow keys
+    var dirx = 0;
+    var diry = 0;
+    if (Keyboard.isDown(Keyboard.LEFT)) { dirx = -1; }
+    if (Keyboard.isDown(Keyboard.RIGHT)) { dirx = 1; }
+    if (Keyboard.isDown(Keyboard.UP)) { diry = -1; }
+    if (Keyboard.isDown(Keyboard.DOWN)) { diry = 1; }
+
+    if (dirx !== 0 || diry !== 0) {
+        this.camera.move(delta, dirx, diry);
+        this.hasScrolled = true;
+    }
+};
+
+Game._drawMap = function () {
+    map.layers.forEach(function (layer, index) {
+        this._drawLayer(index);
+    }.bind(this));
 };
 
 Game._drawLayer = function (layer) {
-    for (var c = 0; c < map.cols; c++) {
-        for (var r = 0; r < map.rows; r++) {
+    var context = this.layerCanvas[layer].getContext('2d');
+    context.clearRect(0, 0, 512, 512);
+
+    var startCol = Math.floor(this.camera.x / map.tsize);
+    var endCol = startCol + (this.camera.width / map.tsize);
+    var startRow = Math.floor(this.camera.y / map.tsize);
+    var endRow = startRow + (this.camera.height / map.tsize);
+    var offsetX = -this.camera.x + startCol * map.tsize;
+    var offsetY = -this.camera.y + startRow * map.tsize;
+
+    for (var c = startCol; c <= endCol; c++) {
+        for (var r = startRow; r <= endRow; r++) {
             var tile = map.getTile(layer, c, r);
+            var x = (c - startCol) * map.tsize + offsetX;
+            var y = (r - startRow) * map.tsize + offsetY;
             if (tile !== 0) { // 0 => empty tile
-                this.ctx.drawImage(
+                context.drawImage(
                     this.tileAtlas, // image
                     (tile - 1) * map.tsize, // source x
                     0, // source y
                     map.tsize, // source width
                     map.tsize, // source height
-                    c * map.tsize,  // target x
-                    r * map.tsize, // target y
+                    Math.round(x),  // target x
+                    Math.round(y), // target y
                     map.tsize, // target width
                     map.tsize // target height
                 );
@@ -61,10 +134,12 @@ Game._drawLayer = function (layer) {
 };
 
 Game.render = function () {
-    // draw map background layer
-    this._drawLayer(0);
-    // draw game sprites
-    this.ctx.drawImage(this.hero.image, this.hero.x, this.hero.y);
-    // draw map top layer
-    this._drawLayer(1);
+    // re-draw map if there has been scroll
+    if (this.hasScrolled) {
+        this._drawMap();
+    }
+
+    // draw the map layers into game context
+    this.ctx.drawImage(this.layerCanvas[0], 0, 0);
+    this.ctx.drawImage(this.layerCanvas[1], 0, 0);
 };
