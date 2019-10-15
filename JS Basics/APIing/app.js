29452b0d22714bd3e33378c96a689effb5e3cbac
Kori <icedragon668@gmail.com>
2019-10-14 22:52:10 -0400

    adds some detail from response

--- JS Basics/APIing/app.js
+++ JS Basics/APIing/app.js
@@ -32,10 +32,17 @@ fetch(queryURL)
         <h1>${res.name}<h1>`)
         main('main').write(`
         <ul>
-        <li>temp: ${res.main.temp}</li>
+        <li>temp: ${res.main.temp}K</li>
         <li>pressure: ${res.main.pressure}</li>
         <li>humidity: ${res.main.humidity}</li>
         </ul>`)
+        const tempC = (res.main.temp - 273.15)
+        const tempF = tempC*1.80 +32
+        main('main').write(`
+        <br>
+        <p>temp is ${Math.round(tempF)}F</p>
+        <p>and ${Math.round(tempC)}C</p>`
+        ,"+")
         console.log(res)
     })
     .catch(function(err){
