be411a05a031bc455f6ac63ff57a26fdea487c1d
Kori <icedragon668@gmail.com>
2019-10-17 22:26:35 -0400

    adds new files to lesson, operations.js

---
+++ JS Basics/Node and Exports/operations.js
@@ -0,0 +1,9 @@
+/* export these functions
+The value of x is 3 and the value of y is 4
+The sum of 3 and 4 is 7
+The difference of 3 and 4 is -1
+The product of 3 and 4 is 12
+The quotient of 3 and 4 is 0.75
+The remainder of 3 divided by 4 is 3
+The value of 3 raised to the 4 power is 81
+*/
