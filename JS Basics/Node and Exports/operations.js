358a700d87d3bafa6332ab8d52dbf71815c8f05e
Kori <icedragon668@gmail.com>
2019-10-17 22:57:16 -0400

    fixes missing return and coverts number to parefloat for multiply

--- JS Basics/Node and Exports/operations.js
+++ JS Basics/Node and Exports/operations.js
@@ -11,7 +11,7 @@ The value of 3 raised to the 4 power is 81
 module.exports = {
 add: (x,y) => { return parseFloat(x + y) },
 subtract: (x,y) => { return parseFloat(x - y) },
-multiply: (x,y) => { Number(x * y) },
+multiply: (x,y) => { return parseFloat(x * y) },
 divide: (x,y) => { Number(x / y) },
 remainder: (x,y) => { Number(x % y) },
 exp: (x,y) => { Number(x ** y) },
