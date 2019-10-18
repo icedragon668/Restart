e3e27ef61f86d22fdd4e2c7ca40de50dda546cb1
Kori <icedragon668@gmail.com>
2019-10-17 22:37:23 -0400

    adds exports to operations.js

--- JS Basics/Node and Exports/operations.js
+++ JS Basics/Node and Exports/operations.js
@@ -7,3 +7,12 @@ The quotient of 3 and 4 is 0.75
 The remainder of 3 divided by 4 is 3
 The value of 3 raised to the 4 power is 81
 */
+
+module.exports = {
+add,
+subtract,
+multiply,
+divide,
+remainder,
+exp,
+}
