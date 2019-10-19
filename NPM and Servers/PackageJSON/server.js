300ed26d01139a0ade56c265cc54ce09e8447c15
Kori <icedragon668@gmail.com>
2019-10-18 21:59:32 -0400

    adds path dependency, and /home calls index.html

--- NPM and Servers/PackageJSON/server.js
+++ NPM and Servers/PackageJSON/server.js
@@ -1,13 +1,14 @@
 //a basic server
 
 const express = require('express');
+const path = require('path')
 
 const app = express()
 
 const PORT = 8080
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
+app.get('/home', function(req, res){
+    res.sendFile(path.join__dirname, "index.html")
 })
 
 app.get('/author', function (req, res) {
@@ -19,6 +20,10 @@ app.get('/date', function (req, res) {
     let m = date.getMonth() + 1
     let d = date.getDate() + 1
     res.send(`Today is day ${d} of month ${m}`)
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
 })
 
 app.listen(PORT, function () {
