b987508ccc35ef7aee54e3b55cc86c75e7d4830c
Kori <icedragon668@gmail.com>
2019-10-18 21:34:24 -0400

    adds a write function

--- NPM and Servers/PackageJSON/node.js
+++ NPM and Servers/PackageJSON/node.js
@@ -3,10 +3,18 @@ const axios = require('axios')
 
 const fs = require('fs')
 
+//fs read from file
 fs.readFile('items.txt', "utf8", function(e,d) {
     if (e){ return console.log(e) }
     console.log(d)
 
     const dList = d.split(',');
     console.log(dList)
+})
+
+//fs write to file
+fs.writeFile('desktop.txt', 'get coffe, code js', function(err){
+    if (err) { return console.log(err)}
+
+    console.log('file created')
 })
