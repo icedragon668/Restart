9c418973b344b268dc904b81f61e41079e619c29
Kori <icedragon668@gmail.com>
2019-10-18 21:19:59 -0400

    adds a split for readability

--- NPM and Servers/PackageJSON/node.js
+++ NPM and Servers/PackageJSON/node.js
@@ -6,4 +6,7 @@ const fs = require('fs')
 fs.readFile('items.txt', "utf8", function(e,d) {
     if (e){ return console.log(e) }
     console.log(d)
+
+    const dList = d.split(',');
+    console.log(dList)
 })
