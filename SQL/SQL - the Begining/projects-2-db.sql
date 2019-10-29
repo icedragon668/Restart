3bbde3afd101cfdf3b0786fd9b8565e32b9715df
Kori <icedragon668@gmail.com>
2019-10-28 22:52:39 -0400

    adds data to table one

--- SQL/SQL - the Begining/projects-2-db.sql
+++ SQL/SQL - the Begining/projects-2-db.sql
@@ -1,34 +1,43 @@
--- drop, create, use, varchar, integer, decimal
+-- drop, create, use, varchar, integer, decimal --
 
 DROP DATABASE IF EXISTS projects_db ;
---do, type, condition, name
---this could be better logic-ed, for example, run a check
---then, depending on whats found do other stuff
+--do, type, condition, name --
+--this could be better logic-ed, for example, run a check --
+--then, depending on whats found do other stuff --
 
+
+-- Part 1, creating --
 CREATE DATABASE projects_db;
---do, type, name
+--do, type, name --
 
-USE projects_db; --don't forget to use the db!
+USE projects_db; --don't forget to use the db! --
 
 CREATE TABLE customers (
     company_name VARCHAR(50) NOT NULL,
-    --name, restriction / requirements
+    --name, restriction / requirements --
     email VARCHAR(100) NOT NULL,
     primary_contact VARCHAR(30),
     phone_number VARCHAR(20),
-    currently_active BOOLEAN DEFAULT FALSE, --i think that's right? -- yup
+    currently_active BOOLEAN DEFAULT FALSE, --i think that's right? // yup --
     is_ass Boolean  DEFAULT FALSE
 )
 
 CREATE TABLE projects (
     name VARCHAR(100) NOT NULL,
-    company_name VARCHAR(50) NOT NULL, --switch to keyed to table maybe?
-    start_date DATE NOT NULL, --i think this will autofill current date
+    company_name VARCHAR(50) NOT NULL, --switch to keyed to table maybe? --
+    start_date DATE NOT NULL, --i think this will autofill current date --
     due_date DATE
 )
 
 CREATE TABLE invoices (
     invoice_number INTEGER NOT NULL,
-    amount DECIMAL (10,2), --range, decimal points (subtracted from range. ie 12,345,678.00 here)
+    amount DECIMAL (10,2), --range, decimal points (subtracted from range. ie 12,345,678.00 here) --
     paid BOOLEAN DEFAULT FALSE
 )
+
+-- Part 2, adding data --
+INSERT INTO customers (company_name, email, primary_contact, phone_number)
+-- table (fields)
+VALUES ('Umbrella Coporation', 'ospencer@unbrella.corp', 'O.Spencer', '770-555-6080')
+
+
