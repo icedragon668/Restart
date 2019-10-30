be3e520cb78eea80e594c701a8520a5e36bd56ab
Kori <icedragon668@gmail.com>
2019-10-30 19:42:35 -0400

    adds new lesson, More SQL

---
+++ SQL/Flights/flights.sql
@@ -0,0 +1,17 @@
+use flightsdb;
+
+-- 1. A query which returns flight_no, source_airport, and dest_airport for all flights by a specific airline.
+
+-- 2. A query which searches for a specific flight and returns all data for it.
+
+-- 3. A query which returns all data contained within a specific id range.
+
+-- 4. A query which returns the first 20 flights ordered alphabetically by source_airport.
+
+-- ## Challenge:
+
+-- 5. A query which returns all source_airports which appear in our dataset more than once.
+
+-- 6. A query which returns all flights with a source_airport that begins with "C".
+
+-- 7. A query which returns the top 20 dest_airport ordered based on how many times they appear in the dataset.
