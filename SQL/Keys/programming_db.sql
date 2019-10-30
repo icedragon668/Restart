4448042a562e4aa9d0ee852603a25f18db2d64ae
Kori <icedragon668@gmail.com>
2019-10-29 22:03:58 -0400

    creates a table with auto key

--- SQL/Keys/programming_db.sql
+++ SQL/Keys/programming_db.sql
@@ -3,3 +3,10 @@ DROP DATABASE IF EXISTS programming_db;
 CREATE DATABASE programming_db;
 
 USE programming_db;
+
+CREATE TABLE programming_languages (
+    id Integer AUTO_INCREMENT PRIMARY KEY,
+    language_ VARCHAR(20),
+    rating INTEGER,
+    mastered BOOLEAN DEFAULT true,
+);
