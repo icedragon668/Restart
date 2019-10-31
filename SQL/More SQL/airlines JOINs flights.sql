f621dafb20e9fb0c66cd990f5a7fb26b315b59cc
Kori <icedragon668@gmail.com>
2019-10-30 21:42:04 -0400

    adds code for query 2

--- SQL/More SQL/airlines JOINs flights.sql
+++ SQL/More SQL/airlines JOINs flights.sql
@@ -17,6 +17,13 @@ SELECT flight_no, source_airport, dest_airport
 
 -- 2. You are creating an application where users can search for a specific source airport in the `flights` table that returns the airline name and the destination airport. Write the query that will be able to accomplish this.
 
+SELECT airlines.airline, dest_airport
+    FROM flights
+    LEFT JOIN airlines
+    ON flights.airline = airlines.id
+    WHERE source_airport = 'ABY'
+    --GROUP BY is not needed here
+
 -- ## Challenge:
 
 -- 3. Tanner's Travels, an international travel agency, is interested in contracting you to help them with their most popular destinations list per airline. Construct a query that returns the airline name, destination airport, and the count of the number of flights to that airport, sorted first by the airline, then by the number of flights in descending order.
