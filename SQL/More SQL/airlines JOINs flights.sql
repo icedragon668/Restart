63b557fb2cdbd5f4ac926269564c3c9f1f6188e9
Kori <icedragon668@gmail.com>
2019-10-31 21:24:31 -0400

    adds instructions for next section of lesson

--- SQL/More SQL/airlines JOINs flights.sql
+++ SQL/More SQL/airlines JOINs flights.sql
@@ -1,6 +1,6 @@
 use flightsdb;
 
--- **Scenarios**
+-- Section 1
 
 -- 1. A client is requesting that you create a query that will return the flight number, source airport, and destination airport for a specific named airline, e.g. `JetBlue Airways`. Create the query to test that it actually works as intended.
 SELECT flight_no, source_airport, dest_airport
@@ -24,8 +24,6 @@ SELECT airlines.airline, dest_airport
     WHERE source_airport = 'ABY'
     --GROUP BY is not needed here
 
--- ## Challenge:
-
 -- 3. Tanner's Travels, an international travel agency, is interested in contracting you to help them with their most popular destinations list per airline. Construct a query that returns the airline name, destination airport, and the count of the number of flights to that airport, sorted first by the airline, then by the number of flights in descending order.
 
 --count
@@ -35,3 +33,12 @@ SELECT airlines.airline, dest_airport, count(*) AS Count
     ON flights.airline = airlines.id
     GROUP BY airlines.airline, dest_airport
     ORDER BY airlines.airline, Count DESC;
+
+
+--Section 2
+
+-- 1. Create a query to retrieve the flight number, airline, and source airport name for all flights for the airline, `Delta Airlines`.
+
+-- 2. Create a query to retrieve the flight number, airline, and destination airport city for all flights with the source airport code, `AKK`.
+
+-- 3. Create a query to retrieve the flight number, airline, source airport name, and destination airport name for all flights in our database.
