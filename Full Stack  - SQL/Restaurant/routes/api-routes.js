9f50ec8c624db4767a55e6999ecc1826d20f1344
Kori <icedragon668@gmail.com>
2019-11-01 23:34:00 -0400

    adds post route to api

--- Full Stack  - SQL/Restaurant/routes/api-routes.js
+++ Full Stack  - SQL/Restaurant/routes/api-routes.js
@@ -4,4 +4,13 @@ let waitingList = require('../data/waiting-list')
 module.exports = (app) => {
     app.get('/api/waitinglist', (req,res) => { res.json(waitingList) })
     app.get('/api/tables', (req,res) => { res.json(tableList) })
+
+    app.post('/api/tables', (req, res) => {
+        if (tableList.length < 5) {
+            tableList.push(req.body)
+        } else {
+            waitingList.push(req.body)
+        }
+        res.end()
+    })
 }
