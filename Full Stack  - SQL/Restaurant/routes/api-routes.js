42891051e7e4564de6479ca7ba71ea33af26ebb9
Kori <icedragon668@gmail.com>
2019-11-01 23:34:30 -0400

    adds notation for route

--- Full Stack  - SQL/Restaurant/routes/api-routes.js
+++ Full Stack  - SQL/Restaurant/routes/api-routes.js
@@ -5,6 +5,11 @@ module.exports = (app) => {
     app.get('/api/waitinglist', (req,res) => { res.json(waitingList) })
     app.get('/api/tables', (req,res) => { res.json(tableList) })
 
+    /*
+    when a table gets POSTed, check to see if there are < 5
+    yes: req goes to tables
+    no: req goes to waiting
+    */
     app.post('/api/tables', (req, res) => {
         if (tableList.length < 5) {
             tableList.push(req.body)
