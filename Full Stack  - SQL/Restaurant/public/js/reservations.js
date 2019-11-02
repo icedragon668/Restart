4613e048dd77e729fe915311223b0b6f8c6c3852
Kori <icedragon668@gmail.com>
2019-11-01 23:57:05 -0400

    adds vale grab to object in reservation.js

--- Full Stack  - SQL/Restaurant/public/js/reservations.js
+++ Full Stack  - SQL/Restaurant/public/js/reservations.js
@@ -3,7 +3,10 @@ $(function () {
         e.preventDefault()
 
         const newReservation = {
-            
+            customerName: $('#reserve-name').val().trim(),
+            phoneNumber: $('#reserve-phone').val().trim(),
+            customerEmail: $('#reserve-email').val().trim(),
+            customerID: $('#reserve-unique-id').val().trim()
         }
     }
 
