f2d3b8eb218f2d294fd572033ca597a127705572
Kori <icedragon668@gmail.com>
2019-11-04 23:47:28 -0500

    maybe solves the 12 o'clock issue

--- Fun Stuff/Clocks/app.js
+++ Fun Stuff/Clocks/app.js
@@ -6,37 +6,31 @@ const hrHand = document.querySelector('.hour');
 setDate = function () {
     const now = new Date();
 
-    const mili = now.getMilliseconds()
     const secs = now.getSeconds();
     const mins = now.getMinutes();
     const hrs = now.getHours();
 
-    const secsDegs = (secs *6) + 90;
-    const minsDegs = (mins * 6) + (secs/10) + 90;
-    const hrsDegs = (hrs *15) + (mins/2) + 90;
+    const secsDegs = (secs * 6) + 90;
+    const minsDegs = (mins * 6) + (secs / 10) + 90;
+    const hrsDegs = (hrs * 15) + (mins / 2) + 90;
 
-    switch (secs){
-        case 59: 
-            secHand.style.background = `green`
-            secHand.style.transform = `rotate(${secsDegs}deg)`
-            break
-        case 0: 
-            secHand.style.transform = `rotate(450deg)`
-            secHand.style.transition = 'none'
-            secHand.style.transform = `rotate(${secsDegs}deg)`
-            secHand.style.transition = 'all 0.3s cubic-bezier(0.18, 2.55, 0, 0.9)'
-            break
-        case 1:
-            secHand.style.background = `red`
-            secHand.style.transform = `rotate(${secsDegs}deg)`
-            break
-        default:
-            secHand.style.transform = `rotate(${secsDegs}deg)`;
-}
+ if (secs == 0) {
+        (secHand.style.transform = `rotate(450deg)`)
+        setTimeout(noneFun, 400)
+        setTimeout(moveUnseen, 600)
+        setTimeout(bezierFun, 800)
+    }
+    else {
+        secHand.style.transform = `rotate(${secsDegs}deg)`;
+    }
 
     minHand.style.transform = `rotate(${minsDegs}deg)`;
     hrHand.style.transform = `rotate(${hrsDegs}deg)`;
 }
+
+noneFun = () => {(secHand.style.transition ='none')}
+moveUnseen = () => {(secHand.style.transform = `rotate(90deg)`)}
+bezierFun = () => {(secHand.style.transition ='all 0.3s cubic-bezier(0.18, 2.55, 0, 0.9)')}
 
 setInterval(setDate, 1000);
 
