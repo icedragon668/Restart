9494230284211a50387253072913400d0c9cb3ea
Kori <icedragon668@gmail.com>
2019-11-04 20:51:34 -0500

    calls empty functions

--- Fun Stuff/Clocks/app.js
+++ Fun Stuff/Clocks/app.js
@@ -1,3 +1,11 @@
 const secHand = document.querySelector('.second');
 const minHand = document.querySelector('.min');
 const hrHand = document.querySelector('.hour');
+
+setDate = function () {
+
+}
+
+setInterval(setDate, 1000);
+
+setDate();
