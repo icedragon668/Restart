667571bbc540a3812499b562dda9e491705fbb93
Kori <icedragon668@gmail.com>
2019-11-04 22:06:53 -0500

    converts possible 12 o'clock solution to switch case

--- Fun Stuff/Clocks/app.js
+++ Fun Stuff/Clocks/app.js
@@ -13,18 +13,20 @@ setDate = function () {
     const minsDegs = (mins * 6) + (secs/10) + 90;
     const hrsDegs = (hrs *15) + (mins/2) + 90;
 
-    if (secs == 0) { 
-        secHand.style.transform = `rotate(360deg)`;
-
-        setTimeout(()=>{
+    switch (secs){
+        case 0: { 
+            secHand.style.transform = `rotate(90deg)`;
             secHand.style.transition = `none`
-        }, 10)
-        setTimeout(()=>{
-            secHand.style.transition = `all 0.3s cubic-bezier(0.18, 2.55, 0, 0.9)`
-            }, 30)
-    }
+        }
+        case 1: {
+            secHand.style.transition = `all 0.3s cubic-bezier(0.18, 2.55, 0, 0.9)`;
+            secHand.style.transform = `rotate(${secsDegs}deg)`;
+        }
+        default: {
+            secHand.style.transform = `rotate(${secsDegs}deg)`;
+        }
+}
 
-    secHand.style.transform = `rotate(${secsDegs}deg)`;
     minHand.style.transform = `rotate(${minsDegs}deg)`;
     hrHand.style.transform = `rotate(${hrsDegs}deg)`;
 }
