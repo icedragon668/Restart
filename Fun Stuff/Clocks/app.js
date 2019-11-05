398c03ac21077ec29e93ca90f6aa78f51a6a76f4
Kori <icedragon668@gmail.com>
2019-11-04 21:36:32 -0500

    resolves the 12 o'clock issue

--- Fun Stuff/Clocks/app.js
+++ Fun Stuff/Clocks/app.js
@@ -13,6 +13,11 @@ setDate = function () {
     const minsDegs = (mins * 6) + (secs/10) + 90;
     const hrsDegs = (hrs *15) + (mins/2) + 90;
 
+    if (secs == 0) { 
+        secHand.style.transition = `none`
+        setTimeout(()=>{secHand.style.transition = `all 0.3s cubic-bezier(0.18, 2.55, 0, 0.9)`}, 30)
+    }
+
     secHand.style.transform = `rotate(${secsDegs}deg)`;
     minHand.style.transform = `rotate(${minsDegs}deg)`;
     hrHand.style.transform = `rotate(${hrsDegs}deg)`;
