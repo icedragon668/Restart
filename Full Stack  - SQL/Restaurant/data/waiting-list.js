d1a06a16523616ce3ee386df528aa00baa64ed14
Kori <icedragon668@gmail.com>
2019-11-05 23:01:50 -0500

    adds dummy data to waiting-list.js

---
+++ Full Stack  - SQL/Restaurant/data/waiting-list.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+    {
+      customerName: "Alfons",
+      customerEmail: "aelric@example.com",
+      customerID: "nobody",
+      phoneNumber: "777-000-0000"
+    }
+  ];
+
+module.exports = waitingArray
