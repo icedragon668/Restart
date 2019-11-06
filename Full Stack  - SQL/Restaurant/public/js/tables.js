cfef41a0768fbcb00606c02a7694f72904e97c1d
Kori <icedragon668@gmail.com>
2019-11-05 22:52:43 -0500

    adds missing .length to render for loop

--- Full Stack  - SQL/Restaurant/public/js/tables.js
+++ Full Stack  - SQL/Restaurant/public/js/tables.js
@@ -1,12 +1,12 @@
 $(function(){
 
     const render = function (dataList, parent) {
-        for (i=0; i<dataList; i++){
+        for (i=0; i<dataList.length; i++){
             const reservation = $('<div>').addClass('box');
             reservation.append(`<h3>${dataList[i].customerName}</h3>`);
             reservation.append(`<p>${dataList[i].customerEmail}</p>`);
             reservation.append(`<p>${dataList[i].phoneNumber}</p>`);
-
+            
             parent.append(reservation)
         }
     }
