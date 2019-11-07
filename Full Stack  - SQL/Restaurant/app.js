41152839a24682c7cd4f42e1b3257fddc916b58c
Kori <icedragon668@gmail.com>
2019-11-06 22:13:34 -0500

    adds a findAll query to app.js

--- Full Stack  - SQL/Restaurant/app.js
+++ Full Stack  - SQL/Restaurant/app.js
@@ -1,3 +1,11 @@
 const db = require('./data/models')
 
-db.sequelize.sync()
+db.sequelize.sync().then(function() {
+    db.Reservation.findAll({
+        where: {
+            email: 'foo@test.com'
+        }
+    }).then(function(data) {
+        console.log(JSON.stringify(data, null, 2)); //why this format?
+    })
+})
