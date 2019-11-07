e0a339412a9d5f3f907f8d512bd4d344ee8c54a3
Kori <icedragon668@gmail.com>
2019-11-06 21:56:46 -0500

    sets models/index up to load files into db object

--- Full Stack  - SQL/Restaurant/data/models/index.js
+++ Full Stack  - SQL/Restaurant/data/models/index.js
@@ -4,7 +4,7 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var basename = path.basename(module.filename); //what's this?
+var basename = path.basename(module.filename); //what's this? / is it a self reference?
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../../config.json");
 var db = {};
@@ -14,3 +14,13 @@ if (config.use_env_variable) {
 } else {
     var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+fs
+    .readdirSync(__dirname)
+    .filter(function (file) {
+        return (file.indexOf(".") !== 0 && (file !== basename) && (file.slice(-3) === ".js"));
+    })
+    .forEach(function (file) {
+        var model = sequelize["import"](path.join(__dirname, file));
+        db[model.name] = model;
+    });
