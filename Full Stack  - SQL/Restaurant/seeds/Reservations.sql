7ae37c1635e661c295a29a7fde50b58812288831
Kori <icedragon668@gmail.com>
2019-11-06 22:20:27 -0500

    corrects a backtick vs parentheses issue

--- Full Stack  - SQL/Restaurant/seeds/Reservations.sql
+++ Full Stack  - SQL/Restaurant/seeds/Reservations.sql
@@ -7,14 +7,14 @@ USE restaurant_db
 
 DROP TABLE IF EXISTS Reservations
 
-INSERT INTO Reservations ("name", `phoneNumber`, `email`, `createdAt`, `updatedAt`) VALUES ('Roger', '444-666-3333', 'test@test.com', NOW(), NOW());
+INSERT INTO Reservations ("name", "phoneNumber", "email", "createdAt", "updatedAt") VALUES ('Roger', '444-666-3333', 'test@test.com', NOW(), NOW());
 
-INSERT INTO Reservations (`name`, `phoneNumber`, `email`, `createdAt`, `updatedAt`) VALUES ('Lillian', '444-666-3377', 'sample@test.com', NOW(), NOW());
+INSERT INTO Reservations ("name", "phoneNumber", "email", "createdAt", "updatedAt") VALUES ('Lillian', '444-666-3377', 'sample@test.com', NOW(), NOW());
 
-INSERT INTO Reservations (`name`, `phoneNumber`, `email`, `createdAt`, `updatedAt`) VALUES ('Lillian', '555-555-5555', 'random@test.com', NOW(), NOW());
+INSERT INTO Reservations ("name", "phoneNumber", "email", "createdAt", "updatedAt") VALUES ('Lillian', '555-555-5555', 'random@test.com', NOW(), NOW());
 
-INSERT INTO Reservations (`name`, `phoneNumber`, `email`, `createdAt`, `updatedAt`) VALUES ('Jaylin', '444-666-3377', 'another@test.com', NOW(), NOW());
+INSERT INTO Reservations ("name", "phoneNumber", "email", "createdAt", "updatedAt") VALUES ('Jaylin', '444-666-3377', 'another@test.com', NOW(), NOW());
 
-INSERT INTO Reservations (`name`, `phoneNumber`, `email`, `createdAt`, `updatedAt`) VALUES ('Jay', '777-777-7777', 'thisone@test.com', NOW(), NOW());
+INSERT INTO Reservations ("name", "phoneNumber", "email", "createdAt", "updatedAt") VALUES ('Jay', '777-777-7777', 'thisone@test.com', NOW(), NOW());
 
-INSERT INTO Reservations (`name`, `phoneNumber`, `email`, `createdAt`, `updatedAt`) VALUES ('Phil', '999-999-9999', 'foo@test.com', NOW(), NOW());
+INSERT INTO Reservations ("name", "phoneNumber", "email", "createdAt", "updatedAt") VALUES ('Phil', '999-999-9999', 'foo@test.com', NOW(), NOW());
