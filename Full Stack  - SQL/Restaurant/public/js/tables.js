c0987cd189a7bfdfd084b6d6dd279c598f55090e
Kori <icedragon668@gmail.com>
2019-11-06 21:16:55 -0500

    adds an event delegtor function

--- Full Stack  - SQL/Restaurant/public/js/tables.js
+++ Full Stack  - SQL/Restaurant/public/js/tables.js
@@ -1,33 +1,36 @@
-    const render = function (dataList, parent) {
-        for (i=0; i<dataList.length; i++){
-            const reservation = `<div class="box">
+const render = function (dataList, parent) {
+    for (i = 0; i < dataList.length; i++) {
+        const reservation = `<div class="box">
             <h3>${dataList[i].customerName}</h3>
             <p>${dataList[i].customerEmail}</p>
             <p>${dataList[i].phoneNumber}</p></div>`
-            document.querySelector(parent).innerHTML+=(reservation)
-        }
+        document.querySelector(parent).innerHTML += (reservation)
     }
+}
 
-    const getTables = function(){
-        fetch('api/tables', {method:'GET'}).then(data=>data.json())
-        .then(function(data){
+const getTables = function () {
+    fetch('api/tables', { method: 'GET' }).then(data => data.json())
+        .then(function (data) {
             render(data, '.tables')
         })
-    }
+}
 
-    const getWaitList = function(){
-        fetch('api/waitinglist', {method:'GET'}).then(data=>data.json())
-        .then(function(data){
+const getWaitList = function () {
+    fetch('api/waitinglist', { method: 'GET' }).then(data => data.json())
+        .then(function (data) {
             render(data, '.waitlist');
         })
-    }
+}
 
-    const clearAll = function() {
+const clearAll = function (e) {
+    e.preventDefault()
+    if (e.target.matches('.btn')) {
         document.querySelector('.tables').innerHTML = ''
         document.querySelector('.waitlist').innerHTML = ''
     }
+}
 
-    getTables()
-    getWaitList()
-//not quite
-    document.querySelector('.danger').addEventListener('click', clearAll)
+getTables()
+getWaitList()
+
+document.addEventListener('click', clearAll)
