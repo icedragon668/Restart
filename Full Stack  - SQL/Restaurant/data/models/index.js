aa6b7a9a8f7ebf150f328660969d5a3457f30762
Kori <icedragon668@gmail.com>
2019-11-06 21:50:24 -0500

    sets up models/index for deploy || dev environments

--- Full Stack  - SQL/Restaurant/data/models/index.js
+++ Full Stack  - SQL/Restaurant/data/models/index.js
@@ -8,3 +8,9 @@ var basename = path.basename(module.filename); //what's this?
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../../config.json");
 var db = {};
+
+if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
