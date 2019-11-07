55981809f818131699b5248c82b82bb59355907c
Kori <icedragon668@gmail.com>
2019-11-06 22:24:59 -0500

    alters and adds note to the seed

--- Full Stack  - SQL/Restaurant/seeds/Reservations.sql
+++ Full Stack  - SQL/Restaurant/seeds/Reservations.sql
@@ -1,12 +1,15 @@
 -- i need to check the database..., or just make it up as i go :p
-DROP DATABASE IF EXISTS restaurant_db
+-- DROP DATABASE IF EXISTS restaurant_db
 
-CREATE DATABASE restaurant_db
+-- CREATE DATABASE restaurant_db
 
 USE restaurant_db
 
-DROP TABLE IF EXISTS Reservations
+-- DROP TABLE IF EXISTS Reservations
+
+-- CREATE TABLE Reservations ("name" Varchar(30), "phoneNumber" Varchar(30), "email" Varchar(30), "createdAt" Varchar(30), "updatedAt" Varchar(30))
 -- is there maybe a USE IF EXISTS, ELSE CREATE syntax i can use?
+-- also, is this built right?, doesn't the models/index make this?
 
 INSERT INTO Reservations ("name", "phoneNumber", "email", "createdAt", "updatedAt") VALUES ('Roger', '444-666-3333', 'test@test.com', NOW(), NOW());
 
