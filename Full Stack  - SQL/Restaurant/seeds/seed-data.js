e8a03e904ed3e9146e22077c4b12f069e32f570a
Kori <icedragon668@gmail.com>
2019-11-07 20:39:06 -0500

    codes data for bulkCreate

--- Full Stack  - SQL/Restaurant/seeds/seed-data.js
+++ Full Stack  - SQL/Restaurant/seeds/seed-data.js
@@ -2,3 +2,22 @@ const db = require('./../data/models');
 
 db.sequelize.sync().then(()=>{
     db.Reservation.bulkCreate([{
+        name: 'Katherine',
+        phoneNumber: '222-555-2290',
+        email: 'kat@prodigy.org'
+      }, {
+        name: 'Chuck',
+        phoneNumber: '912-770-0404',
+        email: 'chuck@gmail.com'
+      }, {
+        name: 'Raj',
+        phoneNumber: '478-229-7060',
+        email: 'raj@yahoo.com'
+      }, {
+        name: 'Daniel',
+        phoneNumber: '0118-999-811-99',
+        email: 'daniel@lycos.com'
+      }, {
+        name: 'Hannah',
+        phoneNumber: '1-800-COLLECT',
+        email: 'hannah@compuserve.net'
