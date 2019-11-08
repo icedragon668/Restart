4af8807b6c82d77bfc7f0b4bc82577634665e562
Kori <icedragon668@gmail.com>
2019-11-07 20:50:36 -0500

    formats and adds notes to app.js

--- Full Stack  - SQL/Restaurant/app.js
+++ Full Stack  - SQL/Restaurant/app.js
@@ -1,6 +1,8 @@
 const db = require('./data/models')
 
+//findall(where: )
 db.sequelize.sync().then(function() {
+
     db.Reservation.findAll({
         where: {
             email: 'foo@test.com'
@@ -8,4 +10,5 @@ db.sequelize.sync().then(function() {
     }).then(function(data) {
         console.log(JSON.stringify(data, null, 2)); //why this format?
     })
+
 })
