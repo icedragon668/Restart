9ca8a6c4cfcac2883e65158278ead0154b4b1bb7
Kori <icedragon668@gmail.com>
2019-11-08 21:58:55 -0500

    preps api-routes for sql database

--- Full Stack  - SQL/Restaurant/routes/api-routes.js
+++ Full Stack  - SQL/Restaurant/routes/api-routes.js
@@ -1,3 +1,4 @@
+/* Code for data.json on the server
 let tableList = require('../data/table-list')
 let waitingList = require('../data/waiting-list')
 
@@ -5,11 +6,11 @@ module.exports = (app) => {
     app.get('/api/waitinglist', (req,res) => { res.json(waitingList) })
     app.get('/api/tables', (req,res) => { res.json(tableList) })
 
-    /*
+    /
     when a table gets POSTed, check to see if there are < 5
     yes: req goes to tables
     no: req goes to waiting
-    */
+    /
     app.post('/api/tables', (req, res) => {
         if (tableList.length < 5) {
             tableList.push(req.body)
@@ -19,3 +20,7 @@ module.exports = (app) => {
         res.end()
     })
 }
+End Code for data.json */
+
+// Code for SQL database
+
