566108657d49932093930fa9304e1ab15b6bbbaa
Kori <icedragon668@gmail.com>
2019-11-08 21:49:35 -0500

    adjusts server.js for sql database

--- Full Stack  - SQL/Restaurant/server.js
+++ Full Stack  - SQL/Restaurant/server.js
@@ -1,18 +1,29 @@
+//import express (and path)
 const express = require('express')
 const path = require('path')
 
 const app = express();
 
-const PORT = 8080;
+//Port for deploy/dev
+const PORT = process.env.PORT || 8080;
 
+//parser req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//path to public files
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Routes
 require('./routes/api-routes.js')(app);
 require('./routes/html-routes.js')(app);
 
-app.listen(PORT, function(){
-    console.log(`Listening on Port ${PORT}`)
+//database sync
+const db = require('./data/models');
+db.sequelize.sync().then(() => {
+
+    //starts server if sync success
+    app.listen(PORT, function () {
+        console.log(`Listening on Port ${PORT}`)
+    })
 })
