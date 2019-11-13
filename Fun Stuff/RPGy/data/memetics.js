8c17dca7fde10cfe0fbcd8bfef3175bee8051487
Kori <icedragon668@gmail.com>
2019-11-12 23:12:20 -0500

    adds some character ideas

--- Fun Stuff/RPGy/data/memetics.js
+++ Fun Stuff/RPGy/data/memetics.js
@@ -9,3 +9,18 @@ const Word = {
     saturation: ['how common is this word?','linked to location'],
     accuracy: 'how accurate is the word to the truth?'
 }
+
+const Character = {
+    name: 'name',
+    location: 'general, not specfic',
+    routine: {
+        location: "specfic",
+        time: "at this time"
+    },
+    memetics: {
+        learnAccuracy: "how accurately do they learn something",
+        learnRate: "how likley are they to pick up something new?",
+        recountRate: "how likely are they to bring up a new idea?",
+        recountAccuracy: "how accurately do they recount the new idea?"
+    }
+}
