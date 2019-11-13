59e022895e1a96168833c0421d36314986417f81
Kori <icedragon668@gmail.com>
2019-11-12 23:34:14 -0500

    changes const word to class

--- Fun Stuff/RPGy/data/memetics.js
+++ Fun Stuff/RPGy/data/memetics.js
@@ -2,31 +2,37 @@
 
 //now would be a good time to review classes lol
 
-const Word = {
-    word: 'what is this word?',
-    origin: 'where did this word start?',
-    truth: "what is really going on here?",
-    locations: ['places this word has been heard'],
-    saturation: ['how common is this word?','linked to location'],
-    variations: [{params: "params based on the word(?)", variations: "the alternate truth"}]
+class Word {
+    constructor(word, origin, truth, nodes, variations) {
+        this.word = 'what is this word?',
+            this.origin = 'where did this word start?',
+            this.truth = "what is really going on here?",
+            this.nodes = [{
+                location: "heard here",
+                saturation: "this much"
+            }]
+            this.variations = [{ params: "params based on the word(?)", variations: "the alternate truth" }]
+    }
 }
 //the variations object needs work...
 
+
 class Character {
     constructor(name, location, routine, memetics) {
-    this.name= 'name',
-    this.location= 'general, not specfic',
-    this.routine= [{
-        location: "specfic",
-        time: "at this time"
-    }],
-    this.memetics= {
-        learnAccuracy: "how accurately do they learn something",
-        learnRate: "how likley are they to pick up something new?",
-        recountRate: "how likely are they to bring up a new idea?",
-        recountAccuracy: "how accurately do they recount the new idea?"
+        this.name = 'name',
+            this.location = 'general, not specfic',
+            this.routine = [{
+                location: "specfic",
+                time: "at this time"
+            }],
+            this.memetics = {
+                learnAccuracy: "how accurately do they learn something",
+                learnRate: "how likley are they to pick up something new?",
+                recountRate: "how likely are they to bring up a new idea?",
+                recountAccuracy: "how accurately do they recount the new idea?"
+            }
     }
-}}
+}
 
 /* Example Set 1 - memetics
 the char Foli is only good at learning accuracy, they do not offer new info, they don't observe new things, and they mis-remember (create new variants)
@@ -37,8 +43,8 @@ const Foli = new Character(
     'Foli',
     'Town',
     [
-        {location: 'Town', time: 'day'},
-        {location: 'Bed', time: 'night'}
+        { location: 'Town', time: 'day' },
+        { location: 'Bed', time: 'night' }
     ],
     {
         learnAccuracy: 100,
@@ -52,8 +58,8 @@ const Teki = new Character(
     'Teki',
     'Town',
     [
-        {location: 'Town', time: 'night'},
-        {location: 'Bed', time: 'day'}
+        { location: 'Town', time: 'night' },
+        { location: 'Bed', time: 'day' }
     ],
     {
         learnAccuracy: 10,
