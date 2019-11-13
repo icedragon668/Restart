9c8544419c2a98fd5028fd227fa9e8056df08170
Kori <icedragon668@gmail.com>
2019-11-12 23:08:09 -0500

    begins idea-crafting for the word/rumor mechanic

--- Fun Stuff/RPGy/data/memetics.js
+++ Fun Stuff/RPGy/data/memetics.js
@@ -1,3 +1,11 @@
 //this is to be restructured as db info. probably sql. but i need to get the ides fleshed out first
 
 //now would be a good time to review classes lol
+
+const Word = {
+    word: 'what is this word?',
+    origin: 'where did this word start?',
+    locations: ['places this word has been heard'],
+    saturation: ['how common is this word?','linked to location'],
+    accuracy: 'how accurate is the word to the truth?'
+}
