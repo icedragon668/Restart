ea690b076f9c9fdc3b5dfe3fbc0783d801d9ad6c
Kori <icedragon668@gmail.com>
2019-11-13 23:26:47 -0500

    notes possible solution in truths.js

--- Fun Stuff/RPGy/data/truths.js
+++ Fun Stuff/RPGy/data/truths.js
@@ -2,3 +2,7 @@ const Locales = [
     {Frostholm: `this is a blurb of varying length. it references itself. eg, ${word} is a ${type} in the ${region}`,
     keywords: ['word', 'type', 'region']}
 ]
+
+//will likely need a different method, eg RegEx
+//maybe instead of vars, use WORD and TYPE etc.
+//then id them via keywords and replace them with RegEx finders
