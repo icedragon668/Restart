4ddf4833500b9ab5ae5f6db714f8e598665d01a0
Kori <icedragon668@gmail.com>
2019-12-09 22:33:24 -0500

    sets up index.js variables

--- Full Stack  - SQL/Literature/models/index.js
+++ Full Stack  - SQL/Literature/models/index.js
@@ -1,1 +1,9 @@
 "use strict";
+
+let fs = require("fs");
+let path = require("path");
+let Sequelize = require("sequalize");
+let basename = path.basename(module.filename); //???
+let env = process.env.NODE_ENV || "development"; //node or dev
+let config = require(__dirname + '../config/config.json')[env]; //which config to use
+let db = {}; //const?
