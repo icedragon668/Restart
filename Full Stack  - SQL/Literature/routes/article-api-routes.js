fb9f5cac680c96e16e1f9a431ee1ddfb4060f444
Kori <icedragon668@gmail.com>
2019-12-09 23:01:16 -0500

    findAll articles

--- Full Stack  - SQL/Literature/routes/article-api-routes.js
+++ Full Stack  - SQL/Literature/routes/article-api-routes.js
@@ -1,5 +1,13 @@
 const db = require('../models'); //gotta have the models
 
 module.exports = (app) => {
+    //GET all articles
+    app.get('.api/articles', (req, res) => {
+        db.Article.findAll({}).then(dbArticle => {
+            res.json(dbArticle);
+        }).catch(err => {
+            res.json({ error: err })
+        })
+    })
     
 }
