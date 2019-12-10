b7f1554a1f837823cab8437720a55eb6fa60a0e4
Kori <icedragon668@gmail.com>
2019-12-09 22:36:51 -0500

    defines production vs develepoment environments

--- Full Stack  - SQL/Literature/models/index.js
+++ Full Stack  - SQL/Literature/models/index.js
@@ -7,3 +7,9 @@ let basename = path.basename(module.filename); //???
 let env = process.env.NODE_ENV || "development"; //node or dev
 let config = require(__dirname + '../config/config.json')[env]; //which config to use
 let db = {}; //const?
+
+if ( config.use_env_variable) {
+    let sequalize = new Sequelize(process.env[config.use_env_variable]); //if not dev, use production
+} else {
+    let sequalize = new Sequelize(config.database, config.username, config.password, config) //is dev, use dev info
+}
