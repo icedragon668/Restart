24e201d18e23a352ee0f1fcdff08d61305657563
Kori <icedragon668@gmail.com>
2019-12-10 23:10:12 -0500

    on success, return the json

--- Full Stack  - SQL/Literature/routes/author-api-route.js
+++ Full Stack  - SQL/Literature/routes/author-api-route.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
     //GET all authors
     app.get('/api/authors', (req, res)=>{
         db.Author.findAll({})
+        .then((dbAuthor)=>{ res.json({ dbAuthor }) })
     })
 
 }
